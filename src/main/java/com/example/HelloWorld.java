e5270174939b419e963ea0cedefe78df8821f1ad
Melted-IceCream <thomasteohjs@gmail.com>
2024-08-19 10:44:49 +0800

    Trigger jenkins pipeline

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -2,10 +2,10 @@ package com.example;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World !");
     }
 
     public String getGreeting() {
-        return "Hello, World!";
+        return "Hello, World !";
     }
 }
